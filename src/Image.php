0249eee3068b912027c23a5c049037053045caf3
快乐浪子哥 <hulangfy@163.com>
2023-05-12 15:57:54 +0800

    Update

--- src/Image.php
+++ src/Image.php
@@ -190,7 +190,7 @@ class Image
 
     /**
      * 翻转图像
-     * @param integer $direction 翻转轴,X或者Y
+     * @param int $direction 翻转轴,X或者Y
      * @return mixed|$this
      */
     public function flip($direction = self::FLIP_X)
@@ -223,12 +223,12 @@ class Image
     /**
      * 裁剪图像
      *
-     * @param  integer $w      裁剪区域宽度
-     * @param  integer $h      裁剪区域高度
-     * @param  integer $x      裁剪区域x坐标
-     * @param  integer $y      裁剪区域y坐标
-     * @param  integer $width  图像保存宽度
-     * @param  integer $height 图像保存高度
+     * @param  int $w      裁剪区域宽度
+     * @param  int $h      裁剪区域高度
+     * @param  int $x      裁剪区域x坐标
+     * @param  int $y      裁剪区域y坐标
+     * @param  int $width  图像保存宽度
+     * @param  int $height 图像保存高度
      *
      * @return mixed|$this
      */
@@ -258,9 +258,9 @@ class Image
     /**
      * 生成缩略图
      *
-     * @param  integer $width  缩略图最大宽度
-     * @param  integer $height 缩略图最大高度
-     * @param int      $type   缩略图裁剪类型
+     * @param int $width  缩略图最大宽度
+     * @param int $height 缩略图最大高度
+     * @param int $type   缩略图裁剪类型
      *
      * @return mixed|$this
      */
@@ -357,7 +357,7 @@ class Image
     /**
      * 添加水印
      *
-     * @param  string $source 水印图片路径
+     * @param string  $source 水印图片路径
      * @param int     $locate 水印位置
      * @param int     $alpha  透明度
      * @return mixed|$this
@@ -453,11 +453,11 @@ class Image
      *
      * @param  string  $text   添加的文字
      * @param  string  $font   字体路径
-     * @param  integer $size   字号
+     * @param  int     $size   字号
      * @param  string  $color  文字颜色
-     * @param int      $locate 文字写入位置
-     * @param  integer $offset 文字相对当前位置的偏移量
-     * @param  integer $angle  文字倾斜角度
+     * @param  int     $locate 文字写入位置
+     * @param  int     $offset 文字相对当前位置的偏移量
+     * @param  int     $angle  文字倾斜角度
      *
      * @return mixed|$this
      * @throws ImageException
